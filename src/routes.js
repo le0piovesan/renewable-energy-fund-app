bcf382746c751f7a15a3b0920f6898fc78387661
Leonardo Piovesan <leopvsn@gmail.com>
2023-02-03 19:01:01 -0300

    Add bottom tabs navigator

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useSelector } from "react-redux";
+import defaultStyle from "./defaultStyle";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 // Pages
 import Login from "./views/Auth/Login";
 import Register from "./views/Auth/Register";
 import Home from "./views/Home";
-import defaultStyle from "./defaultStyle";
+import Portfolio from "./views/Portfolio";
+import Trade from "./views/Trade";
 
 const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default AppContainer = () => {
   const selector = useSelector((state) => state);
@@ -19,22 +24,67 @@ export default AppContainer = () => {
   return (
     <NavigationContainer>
       {currentUser ? (
-        <Stack.Navigator>
-          <Stack.Screen
+        <Tabs.Navigator
+          initialRouteName="Home"
+          lazy={false}
+          screenOptions={{
+            tabBarActiveTintColor: defaultStyle.brandPrimary,
+            style: {
+              height: 70,
+              elevation: 0,
+              shadowOpacity: 0,
+              paddingBottom: 7,
+              paddingTop: 7,
+            },
+          }}
+        >
+          <Tabs.Screen
             name="Home"
             component={Home}
             options={{
-              title: "Home",
               headerStyle: {
                 backgroundColor: defaultStyle.brandPrimary,
               },
-              headerTintColor: "#000",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
+              headerTintColor: "#fff",
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" size={24} color={color} />
+              ),
+              title: "Home",
             }}
           />
-        </Stack.Navigator>
+          <Tabs.Screen
+            name="Trade"
+            component={Trade}
+            options={{
+              headerStyle: {
+                backgroundColor: defaultStyle.brandPrimary,
+              },
+              headerTintColor: "#fff",
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons
+                  name="arrow-decision-outline"
+                  size={24}
+                  color={color}
+                />
+              ),
+              title: "Trade",
+            }}
+          />
+          <Tabs.Screen
+            name="Portfolio"
+            component={Portfolio}
+            options={{
+              headerStyle: {
+                backgroundColor: defaultStyle.brandPrimary,
+              },
+              headerTintColor: "#fff",
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="store" size={24} color={color} />
+              ),
+              title: "Portfolio",
+            }}
+          />
+        </Tabs.Navigator>
       ) : (
         <Stack.Navigator>
           <Stack.Screen
@@ -45,10 +95,7 @@ export default AppContainer = () => {
               headerStyle: {
                 backgroundColor: defaultStyle.brandPrimary,
               },
-              headerTintColor: "#000",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
+              headerTintColor: "#fff",
             }}
           />
           <Stack.Screen
@@ -59,10 +106,7 @@ export default AppContainer = () => {
               headerStyle: {
                 backgroundColor: defaultStyle.brandPrimary,
               },
-              headerTintColor: "#000",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
+              headerTintColor: "#fff",
             }}
           />
         </Stack.Navigator>
